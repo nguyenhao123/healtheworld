6b87dd177d7c575b70d7e16397126ddd618e007a
Hao Nguyen <hnguyen@tampa.healthe>
2019-09-18 14:09:32 -0400

    initial check-in

---
+++ src/main/java/com/healthe/healtheworld/HealtheWorldController.java
@@ -0,0 +1,23 @@
+package com.healthe;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+
+
+
+Simple REST call that says from where its invoked from
+*/
+@RestController
+public class HealtheWorldController {
+
+
+ @GetMapping("/whereami")
+ public String whereami() {
+
+
+ return String.format("Hello from %s", System.getenv().getOrDefault("HOSTNAME", "localhost"));
+ }
+}
