09268f9680e1c6d8d0fde255825a8564431b9cba
Hao Nguyen <hnguyen@tampa.healthe>
2019-09-25 15:53:07 -0400

    testing

--- src/main/java/com/healthe/healtheworld/HealtheWorldController.java
+++ src/main/java/com/healthe/healtheworld/HealtheWorldController.java
@@ -13,12 +13,30 @@ Simple REST call that says from where its invoked from
 @RestController
 public class HealtheWorldController {
 
+String newline = System.getProperty("line.separator");
 
  @GetMapping("/pod")
  public String pod() {
+ 
+ return "HOSTNAME: " + String.format("%s", System.getenv().getOrDefault("HOSTNAME", "localhost"));
+ }
 
-
- return String.format("%s", System.getenv().getOrDefault("HOSTNAME", "localhost"));
+ @GetMapping("/all_env")
+ public String all_env() {
+ 
+ return "HOSTNAME: " + String.format("%s", System.getenv().getOrDefault("HOSTNAME", "localhost")) + "<br>" +
+ "APPLICATION_NAME: " + String.format("%s", System.getenv().getOrDefault("APPLICATION_NAME", "localhost")) + "<br>" +
+ "EnvironmentID: " + String.format("%s", System.getenv().getOrDefault("EnvironmentID", "localhost")) + "<br>" +
+ "DEBUG_LEVEL: " + String.format("%s", System.getenv().getOrDefault("DEBUG_LEVEL", "localhost")) + "<br>" +
+ "DB_HOST: " + String.format("%s", System.getenv().getOrDefault("DB_HOST", "localhost")) + "<br>" +
+ "DB_USER: " + String.format("%s", System.getenv().getOrDefault("DB_USER", "localhost")) + "<br>" +
+ "DB_PASSWORD: " + String.format("%s", System.getenv().getOrDefault("DB_PASSWORD", "localhost")) + "<br>" +
+ "MAIL_HOST: " + String.format("%s", System.getenv().getOrDefault("MAIL_HOST", "localhost")) + "<br>" +
+ "MAIL_USER: " + String.format("%s", System.getenv().getOrDefault("MAIL_USER", "localhost")) + "<br>" +
+ "PERSISTENT_DATA: " + String.format("%s", System.getenv().getOrDefault("PERSISTENT_DATA", "localhost")) + "<br>" +
+ "SPLUNK_HOST: " + String.format("%s", System.getenv().getOrDefault("SPLUNK_HOST", "localhost")) + "<br>" +
+ "SPLUNK_PORT: " + String.format("%s", System.getenv().getOrDefault("SPLUNK_PORT", "localhost")) + "<br>" +
+ "TZ: " + String.format("%s", System.getenv().getOrDefault("TZ", "localhost"));
  }
  
  @GetMapping("/db_host")
@@ -41,21 +59,18 @@ public class HealtheWorldController {
 
  return String.format("%s", System.getenv().getOrDefault("DB_PASSWORD", "letmein999"));
  }
- @GetMapping("/all_env")
-  public String all_env() {
+ 
+ @GetMapping("/application_name")
+  public String application_name() {
 
 
- return String.format("%s", System.getenv().getOrDefault("APPLICATION_NAME", "letmein999")) + "\n" + 
- String.format("%s", System.getenv().getOrDefault("EnvironmentID", "letmein999")) + "\n" +
- String.format("%s", System.getenv().getOrDefault("DEBUG_LEVEL", "letmein999")) + "\n" +
- String.format("%s", System.getenv().getOrDefault("DB_HOST", "letmein999")) + "\n" +
- String.format("%s", System.getenv().getOrDefault("DB_USER", "letmein999")) + "\n" +
- String.format("%s", System.getenv().getOrDefault("DB_PASSWORD", "letmein999")) + "\n" +
- String.format("%s", System.getenv().getOrDefault("MAIL_HOST", "letmein999")) + "\n" +
- String.format("%s", System.getenv().getOrDefault("MAIL_USER", "letmein999")) + "\n" +
- String.format("%s", System.getenv().getOrDefault("PV_MOUNT_PATH", "letmein999")) + "\n" +
- String.format("%s", System.getenv().getOrDefault("SPLUNK_HOST", "letmein999")) + "\n" +
- String.format("%s", System.getenv().getOrDefault("SPLUNK_PORT", "letmein999")) + "\n" +
- String.format("%s", System.getenv().getOrDefault("TZ", "letmein999"));
+ return "APPLICATION_NAME: " + String.format("%s", System.getenv().getOrDefault("APPLICATION_NAME", "letmein999"));
+ }
+
+ @GetMapping("/environment_id")
+  public String environment_id() {
+
+
+ return String.format("%s", System.getenv().getOrDefault("EnvironmentID", "letmein999"));
  }
 }
