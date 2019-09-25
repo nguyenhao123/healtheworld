420b71afbc34b031f6210ac4ba8994096d59c687
Hao Nguyen <hnguyen@tampa.healthe>
2019-09-25 09:57:01 -0400

    test

--- src/main/java/com/healthe/healtheworld/HealtheWorldController.java
+++ src/main/java/com/healthe/healtheworld/HealtheWorldController.java
@@ -21,17 +21,41 @@ public class HealtheWorldController {
  return String.format("%s", System.getenv().getOrDefault("HOSTNAME", "localhost"));
  }
  
+ @GetMapping("/db_host")
+  public String db_host() {
+
+
+ return String.format("%s", System.getenv().getOrDefault("DB_HOST", "localhost"));
+ }
+ 
  @GetMapping("/db_user")
  public String db_user() {
 
 
- return String.format("%s", System.getenv().getOrDefault("DB_USER", "localhost"));
+ return String.format("%s", System.getenv().getOrDefault("DB_USER", "developer"));
  }
  
  @GetMapping("/db_password")
   public String db_password() {
 
 
- return String.format("%s", System.getenv().getOrDefault("DB_PASSWORD", "localhost"));
+ return String.format("%s", System.getenv().getOrDefault("DB_PASSWORD", "letmein999"));
+ }
+ @GetMapping("/all_env")
+  public String all_env() {
+
+
+ return String.format("%s", System.getenv().getOrDefault("APPLICATION_NAME", "letmein999")) + "\n" + 
+ String.format("%s", System.getenv().getOrDefault("EnvironmentID", "letmein999")) + "\n" +
+ String.format("%s", System.getenv().getOrDefault("DEBUG_LEVEL", "letmein999")) + "\n" +
+ String.format("%s", System.getenv().getOrDefault("DB_HOST", "letmein999")) + "\n" +
+ String.format("%s", System.getenv().getOrDefault("DB_USER", "letmein999")) + "\n" +
+ String.format("%s", System.getenv().getOrDefault("DB_PASSWORD", "letmein999")) + "\n" +
+ String.format("%s", System.getenv().getOrDefault("MAIL_HOST", "letmein999")) + "\n" +
+ String.format("%s", System.getenv().getOrDefault("MAIL_USER", "letmein999")) + "\n" +
+ String.format("%s", System.getenv().getOrDefault("PV_MOUNT_PATH", "letmein999")) + "\n" +
+ String.format("%s", System.getenv().getOrDefault("SPLUNK_HOST", "letmein999")) + "\n" +
+ String.format("%s", System.getenv().getOrDefault("SPLUNK_PORT", "letmein999")) + "\n" +
+ String.format("%s", System.getenv().getOrDefault("TZ", "letmein999")) + "\n" +
  }
 }
